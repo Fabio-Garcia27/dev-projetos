499dccee6f1b33036e4a3234bd28a595f5583e15
Fábio Garcia <fabio@unipar.br>
2026-02-19 16:33:36 -0300

    Versão 1.0.0.1

--- chatgpt-clone/tailwindcss.config.js
+++ chatgpt-clone/tailwindcss.config.js
@@ -7,14 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-        backgroundImage: {
-            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-            'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        },
-        colors: {
-            'gpt-gray': '#343541',
-            'gpt-lightgray': '#40414F'
-        }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'gpt-gray': '#343541',
+        'gpt-lightgray': '#40414F'
+      }
     },
   },
   plugins: [],
